88cf3bf18e40f5de63ad23d93306da42584c2eca
NafisAnsari786 <94855409+NafisAnsari786@users.noreply.github.com>
2024-09-05 09:02:32 +0530

    Update and rename Find Runner UP score.txt to Find Runner UP score.py

--- Python Codes/Basic Data Types/Find Runner UP score.py
+++ Python Codes/Basic Data Types/Find Runner UP score.py
@@ -1,12 +1,12 @@
-Given the participants' score sheet for your University Sports Day, you are required to find the runner-up score. You are given  scores. Store them in a list and find the score of the runner-up.
+'''Given the participants' score sheet for your University Sports Day, you are required to find the runner-up score. You are given  scores. Store them in a list and find the score of the runner-up.'''
 
-Sample Input 0
+'''(Sample Input 0
 
 5
 2 3 6 6 5
 Sample Output 0
 
-5
+5'''
 
 
 Solution 1:
